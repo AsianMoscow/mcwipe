14ad2fc969a0040e1edc315817a120070b3ad577
AsianMoscow <theavsv@gmail.com>
2023-12-14 16:43:20 +0300

    token fix

--- botapp.py
+++ botapp.py
@@ -19,7 +19,7 @@ print("TOKEN >" + tg + "<")
 
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = tg
+TOKEN = tg.strip()
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
