5668c4f59f3727afa70b700c84b4fc22fe77d493
AsianMoscow <theavsv@gmail.com>
2023-12-14 18:07:55 +0300

    update.sh

--- botapp.py
+++ botapp.py
@@ -48,6 +48,7 @@ async def echo_handler(message: types.Message) -> None:
     try:
         # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
+        await message.answer('dddd')
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
